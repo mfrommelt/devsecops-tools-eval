f66da663cd9c8f928892e7b13c3b6073331ec33e
mfrommelt <markfrommelt@hotmail.com>
2025-08-14 11:28:37 -0400

    Add files via upload

--- backend/java-springboot/src/main/java/com/api/CommandRequest.java
+++ backend/java-springboot/src/main/java/com/api/CommandRequest.java
@@ -3,7 +3,53 @@ package com.csb.api;
 
 public class CommandRequest {
     private String command;
+    private String arguments;
+    private boolean sudo;
     
-    public String getCommand() { return command; }
-    public void setCommand(String command) { this.command = command; }
+    // Default constructor
+    public CommandRequest() {}
+    
+    // Constructor with parameters
+    public CommandRequest(String command) {
+        this.command = command;
+    }
+    
+    public CommandRequest(String command, String arguments) {
+        this.command = command;
+        this.arguments = arguments;
+    }
+    
+    // Getters and setters
+    public String getCommand() { 
+        return command; 
+    }
+    
+    public void setCommand(String command) { 
+        this.command = command; 
+    }
+    
+    public String getArguments() { 
+        return arguments; 
+    }
+    
+    public void setArguments(String arguments) { 
+        this.arguments = arguments; 
+    }
+    
+    public boolean isSudo() { 
+        return sudo; 
+    }
+    
+    public void setSudo(boolean sudo) { 
+        this.sudo = sudo; 
+    }
+    
+    @Override
+    public String toString() {
+        return "CommandRequest{" +
+                "command='" + command + '\'' +
+                ", arguments='" + arguments + '\'' +
+                ", sudo=" + sudo +
+                '}';
+    }
 }
